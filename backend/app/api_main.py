757a9259988c7d0df3adc6f05f241c8f0716d797
simonlsgr <simon.laschinger@web.de>
2025-12-19 17:03:39 +0100

    fix: imports

--- backend/app/api_main.py
+++ backend/app/api_main.py
@@ -8,7 +8,7 @@ from uuid import UUID
 from fastapi import FastAPI, APIRouter, HTTPException, Depends
 
 from api_models import NurseRosteringJobRequest, NurseRosteringJobStatus
-from solver.solver_skeleton import NurseRosteringModel
+from solver.nurserostering.solver import NurseRosteringModel
 from solver.nurserostering.data_schema import NurseRosteringSolution
 from api_config import get_db_connection, get_task_queue
 from api_tasks import run_optimization_job
