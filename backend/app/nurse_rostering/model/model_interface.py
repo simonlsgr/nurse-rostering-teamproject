7299440309433334b60c8c1c7c1e367ac6203beb
simonlsgr <simon.laschinger@web.de>
2026-01-09 16:11:40 +0100

    feat: add hx model as parameter to accommodate different structure

--- backend/app/nurse_rostering/model/model_interface.py
+++ backend/app/nurse_rostering/model/model_interface.py
@@ -4,28 +4,44 @@ from abc import ABC, abstractmethod
 class ModelInterface(ABC):
 
     @abstractmethod
-    def add_var(self, name, type="bool", lb=None, ub=None):
-        pass
-      
-    @abstractmethod
-    def add_constraint(self, expr, if_var=None):
+    def add_var(self, name, type="bool", lb=None, ub=None, hx_model=None):
         """
-        if_var: optional, bool variable to conditionally enforce this constraint
+        hx_model: optional, is only used for the hexaly implementation to accommodate the different structure.
         """
         pass
       
     @abstractmethod
-    def set_objective(self, expr, sense="min"):
+    def add_constraint(self, expr, if_var=None, hx_model=None):
+        """
+        if_var: optional, bool variable to conditionally enforce this constraint (does not work yet)
+        hx_model: optional, is only used for the hexaly implementation to accommodate the different structure.
+        """
         pass
       
     @abstractmethod
-    def add_max_equality(self, expr_lhs, expr_rhs):
+    def set_objective(self, expr, sense="min", hx_model=None):
+        """
+        hx_model: optional, is only used for the hexaly implementation to accommodate the different structure.
+        """
         pass
       
     @abstractmethod
-    def sum(self, iterable):
+    def add_max_equality(self, expr_lhs, expr_rhs, hx_model=None):
+        """
+        hx_model: optional, is only used for the hexaly implementation to accommodate the different structure.
+        """
         pass
       
     @abstractmethod
-    def get_solution_value(self, var):
+    def sum(self, iterable, hx_model=None):
+        """
+        hx_model: optional, is only used for the hexaly implementation to accommodate the different structure.
+        """
+        pass
+      
+    @abstractmethod
+    def get_solution_value(self, var, hx_model=None):
+        """
+        hx_model: optional, is only used for the hexaly implementation to accommodate the different structure.
+        """
         pass
