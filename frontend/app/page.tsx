bdc24e7ba5a22ec4fc20a26cd57a074491384f96
Valentin Sarezki <v.sarezki@tu-braunschweig.de>
2026-01-30 17:23:56 +0100

    feat: added rough layout for projects menu

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link"
 
 export default function Home() {
+  
+  
+  
   return (
-    <div> Hello.
-      
-      <Link href="/projects"> Projects </Link>  
-    </div>
+      <div>
+        Hallo.
+        <Link href="/projects"> Projects </Link>    
+      </div>
 
   );
 }
--- frontend/app/projects/page.tsx
+++ frontend/app/projects/page.tsx
@@ -5,24 +5,70 @@ import Link from "next/link";
 export default function ProjectsView(){
   
   const projects = [
-    { id: 1, name: "Projekt A" },
-    { id: 2, name: "Projekt B" },
+    { id: 1, name: "Project A", num_of_nurses: 21 },
+    { id: 2, name: "Project B", num_of_nurses: 32 },
   ];
 
   return (
-    <div>
-      {projects.map((project) => (
-        <Link 
-          href={`/projects/${project.id}`}
-          key={project.id}
-        >
-          {project.name}
-        </Link>
-
-      ))}
 
 
+    <div className="flex h-screen gap-2">
+      
+      {/* sidebar left*/}
+      <div className="w-[10vw] min-w-[240px] max-w-[320px] hidden">
+      </div>
+      
+      
+      <div className="w-full">
+      
+        {/* topbar */}
+        <div className="h-[2vw] min-h-[30px] max-h-[40px]">
+        </div>
+      
+        {/* main view */}
+        <div className="p-2">
+      
+          <h1 className="text-4xl font-bold pb-2 mb-4 border-b">
+            Projects
+          </h1>
+      
+          {/* projects list */}
+          <div className="overflow-auto">
+            {projects.map((project) => (
+              <div 
+                className="border rounded-xl w-full h-30 p-2 mb-4"
+                key={project.id}
+              >
+                <Link 
+                    href={`/projects/${project.id}`}
+                    key={project.id}
+                    className="text-xl"
+                >
+                  {project.name}
+                </Link>
+                
+                <div className="pt-4">
+                  <p> ID: {project.id} </p> 
+                  <p> Nurses: {project.num_of_nurses} </p>
+                </div>
+              
+              </div>
+            ))}
+          </div>
+       
+        </div>
+      
+      </div>
 
+      {/* sidebar right */}
+      <div className="w-[1vw]">
+      </div>
+    
     </div>
   );
 }
+
+
+
+
+
