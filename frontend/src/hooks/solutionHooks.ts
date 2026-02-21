cac20cda10573e01dafde15db625e0684f498eea
simonlsgr <simon.laschinger@web.de>
2026-02-21 11:18:07 +0100

    fix: uuid generation

--- frontend/src/hooks/solutionHooks.ts
+++ frontend/src/hooks/solutionHooks.ts
@@ -1,19 +1,15 @@
 import { useSolution } from "@/store/solutionStore";
 import { Solution } from "@/types/nurseVars";
 import solutionData from "@/components/layout/solution_instance_2.json";
+import { nanoid } from "nanoid";
 
 
-function randomId() {
-  console.log("Generating random ID for solution...");
-  return crypto.randomUUID();
-
-}
 
 export function useLoadSolution() {
   const loadSolutionStore = useSolution((s) => s.loadSolution);
 
   const loadSolution = async () => {
-    const id = randomId();
+    const id = nanoid();
     const data = solutionData as unknown as Solution;
     loadSolutionStore(id, data);
   };
