3b268842d39e18c11d2542eea60169ed2bf34200
vsarez <v.sarezki@tu-braunschweig.de>
2026-03-02 22:17:48 +0100

    feat: added icons to NurseList

--- frontend/src/components/rostering/NurseList.tsx
+++ frontend/src/components/rostering/NurseList.tsx
@@ -6,7 +6,9 @@ import NurseCard from "@/components/ui/NurseCard";
 import { Nurse } from "@/types/nurseVars";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-
+import { Search } from 'lucide-react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function NurseList(){
 
@@ -42,16 +44,32 @@ export default function NurseList(){
   return (
 
     <div className="border-b h-[50vh] content-between border-border flex flex-col">
-      <p className="p-2"> All Nurses: </p>
+      <div className="flex justify-between items-center pr-1">
+        <p className="p-2"> All Nurses: </p>
 
-      <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={(s) => setQuery(s.target.value)}
-        className="w-[calc(100%-1rem)] ml-2 p-1 border border-gray-300 rounded focus:outline-none"
-      />
-
+        <div className="flex gap-1">
+          <AddIcon 
+            fontSize="small"
+            className="hover:bg-gray-200 rounded text-muted-foreground"
+          />
+          <MoreVertIcon 
+            fontSize="small"
+            className="hover:bg-gray-200 rounded"          
+          /> 
+        </div>
+      
+      </div>
+      <div className="flex w-[calc(100%-1rem)] ml-2 p-1 border border-gray-300 rounded">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(s) => setQuery(s.target.value)}
+          className="flex-1 focus:outline-none"
+        />
+        <Search fontSize={"small"} />
+      </div>
+      
       <p className="border-b m-2 border-border"></p>
 
       <div className="overflow-auto flex-1">
