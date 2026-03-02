251326e4a634581512125f9479f6ade52a5b716e
vsarez <v.sarezki@tu-braunschweig.de>
2026-03-02 22:38:05 +0100

    feat: added detailview icon to nursecard

--- frontend/src/components/ui/NurseCard.tsx
+++ frontend/src/components/ui/NurseCard.tsx
@@ -1,6 +1,6 @@
 import { useNurseListSelection } from "@/store/nurseStore";
 import { Nurse } from "@/types/nurseVars";
-
+import { ScanSearch } from 'lucide-react';
 
 type NurseCardProps = {
   nurse: Nurse
@@ -15,10 +15,18 @@ export default function NurseCard({ nurse }: NurseCardProps){
   return (
     
     <div 
-      className={`border border-gray-400 rounded-lg w-[calc(100%-1rem)] h-18 p-2 mb-2 ml-2 mt-2 ${selectedNurses.some(n => n.uid === nurse.uid) ? "bg-pink-200 hover:bg-pink-100" : "bg-green-200 hover:bg-green-100"} shadow transition-all duration-170 ease-in-out`}
+      className={`border border-gray-400 rounded-lg w-[calc(100%-1rem)] h-18 p-2 mb-2 ml-2 mt-2 ${selectedNurses.some(n => n.uid === nurse.uid) ? "bg-pink-200 hover:bg-pink-100" : "bg-green-200 hover:bg-green-100"} shadow transition-all duration-170 ease-in-out flex items-center group`}
     >
-      <p className="truncate"> Name: {nurse.name} </p>              
-      <p> ID: {nurse.uid} </p> 
+      <div className="flex-1">
+        <p className="truncate"> Name: {nurse.name} </p>              
+        <p> ID: {nurse.uid} </p> 
+      </div>
+
+      <div>
+        <ScanSearch
+          className="opacity-0 group-hover:opacity-100 transition hover:bg-gray-200 rounded" 
+        />
+      </div>
     </div>
   );
 
