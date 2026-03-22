a6525c6362f72d0866903bce5d5f63891b33f958
simonlsgr <simon.laschinger@web.de>
2026-03-22 20:22:43 +0100

    feat: added solver_ratio & specific parameters

--- backend/app/nurse_rostering/solvers/hybrid/solver.py
+++ backend/app/nurse_rostering/solvers/hybrid/solver.py
@@ -20,30 +20,47 @@ class NurseRosteringModel:
         self,
         log_search_progress: bool = False,
         max_time_in_seconds: float = 60.0,
+        solver_ratio: float = .3,
         **solver_params,
     ) -> NurseRosteringSolution:
         
-        hexaly_time = max_time_in_seconds // 3
+        
+        hexaly_params = {}
+        gurobi_params = {}
+        for key, value in solver_params.items():
+                if key.startswith("hexaly_"):
+                    meta_key = key[len("hexaly_"):]
+                    hexaly_params[meta_key] = value
+                elif key.startswith("gurobi_"):
+                    meta_key = key[len("gurobi_"):]
+                    gurobi_params[meta_key] = value
+                    
+        if solver_ratio > 1 or solver_ratio < 0:
+            raise ValueError("solver_ratio must be in [0,1]!")
+        
+        hexaly_time = int(max_time_in_seconds * solver_ratio)
         gurobi_time = max_time_in_seconds - hexaly_time
         
         hints = {}
+        hxly_sol = None
         if hexaly_time:
             hxly = HXLYSolver(instance=self.instance, formulation=SolverFormulation.TABLE)
             
             
-            hxly_sol = hxly.solve(log_search_progress=log_search_progress, max_time_in_seconds=hexaly_time, **solver_params)
+            hxly_sol = hxly.solve(log_search_progress=log_search_progress, max_time_in_seconds=hexaly_time, **hexaly_params)
             
             if hxly_sol.return_status == SolverReturnStatus.OPTIMAL:
                 return hxly_sol
             elif hxly_sol.return_status == SolverReturnStatus.FEASIBLE:
                 hints = hxly_sol.nurses_at_shifts
         
+        if gurobi_time:
+            grb = GRBSolver(instance=self.instance, hints=hints, formulation=SolverFormulation.IP)
+            
+            grb_sol = grb.solve(log_search_progress=log_search_progress, max_time_in_seconds=gurobi_time, **gurobi_params)
         
-        grb = GRBSolver(instance=self.instance, hints=hints, formulation=SolverFormulation.IP)
-        
-        grb_sol = grb.solve(log_search_progress=log_search_progress, max_time_in_seconds=gurobi_time, **solver_params)
-        
-        return grb_sol
+            return grb_sol
+        return hxly_sol
         
 
 if __name__ == "__main__":
